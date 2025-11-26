c7f38ab019712423ac11526e421800ba554fa0e6
TwangyMoney <87775970+TwangyMoney@users.noreply.github.com>
2025-11-27 00:50:57 +0600

    Refactor TryGetRemoteHeroPosition with PipeClient
    Refactor the TryGetRemoteHeroPosition method to utilize PipeClient for server communication and remove commented-out code.

--- NoskClientAddon.cs
+++ NoskClientAddon.cs
@@ -1,103 +1,39 @@
-﻿#pragma warning disable 1591
+#pragma warning disable 1591
 using Hkmp.Api.Client;
+using HkmpPouch;
 using UnityEngine;
-using System.Reflection;
 
 namespace Nosk_Transformation.HKMP
 {
     public class NoskClientAddon : ClientAddon
     {
         private static IClientApi _clientApi;
+        private static PipeClient _pouchClient;
 
         public override void Initialize(IClientApi clientApi)
         {
             _clientApi = clientApi;
+
             new NoskClientNet(Logger, this, clientApi);
-        }
 
-        public static bool TryGetRemoteHeroPosition(ushort playerId, out Vector3 pos)
-        {
-            pos = default;
-
-            try
+            _pouchClient = new PipeClient("Nosk_Transformation");
+            _pouchClient.ServerCounterPartAvailable(available =>
             {
-                var cm = _clientApi?.ClientManager;
-                if (cm != null)
-                {
-                    var cmType = cm.GetType();
-                    var pm = cmType.GetProperty("PlayerManager")?.GetValue(cm);
-                    if (pm != null)
-                    {
-                        var pmType = pm.GetType();
-                        var getPlayer = pmType.GetMethod("GetPlayer", BindingFlags.Public | BindingFlags.Instance);
-                        var remote = getPlayer?.Invoke(pm, new object[] { playerId });
-
-                        if (remote != null)
-                        {
-                            var rt = remote.GetType();
-
-                            var goProp = rt.GetProperty("GameObject", BindingFlags.Public | BindingFlags.Instance);
-                            if (goProp != null)
-                            {
-                                var go = goProp.GetValue(remote) as GameObject;
-                                if (go != null)
-                                {
-                                    pos = go.transform.position;
-                                    return true;
-                                }
-                            }
-
-                            var getGo = rt.GetMethod("GetGameObject", BindingFlags.Public | BindingFlags.Instance);
-                            if (getGo != null)
-                            {
-                                var go = getGo.Invoke(remote, null) as GameObject;
-                                if (go != null)
-                                {
-                                    pos = go.transform.position;
-                                    return true;
-                                }
-                            }
-
-                            var posProp = rt.GetProperty("Position", BindingFlags.Public | BindingFlags.Instance);
-                            if (posProp != null)
-                            {
-                                var v = posProp.GetValue(remote);
-                                if (v is Vector3 v3)
-                                {
-                                    pos = v3;
-                                    return true;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            catch { }
-
-            try
-            {
-                string pidStr = playerId.ToString();
-                foreach (var t in GameObject.FindObjectsOfType<Transform>())
-                {
-                    var go = t?.gameObject;
-                    if (go == null || !go.activeInHierarchy) continue;
-                    var n = go.name;
-                    if (string.IsNullOrEmpty(n)) continue;
-
-                    bool looksKnight = n.IndexOf("Knight", System.StringComparison.OrdinalIgnoreCase) >= 0
-                                    || n.IndexOf("Player", System.StringComparison.OrdinalIgnoreCase) >= 0
-                                    || n.IndexOf("Remote", System.StringComparison.OrdinalIgnoreCase) >= 0;
-
-                    if (looksKnight && n.IndexOf(pidStr, System.StringComparison.OrdinalIgnoreCase) >= 0)
-                    {
-                        pos = t.position;
-                        return true;
-                    }
-                }
-            }
-            catch { }
-
-            return false;
+                //try
+                //{
+                    //if (available)
+                    //{
+                        //_clientApi.UiManager.ChatBox.AddMessage("Nosk Transformation network active on this server");
+                        //Modding.Logger.Log("[Nosk_Transformation] Server counterpart available (PipeServer present).");
+                    //}
+                    //else
+                    //{
+                        //_clientApi.UiManager.ChatBox.AddMessage("Nosk Transformation is not active on this server");
+                        //Modding.Logger.Log("[Nosk_Transformation] Server counterpart not available (PipeServer missing or failed to load).");
+                    //}
+                //}
+                //catch { }
+            });
         }
 
         protected override string Name => "Nosk_Transformation";
